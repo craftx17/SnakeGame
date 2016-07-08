60abc8590d409dc0db04f131c19cf261867a9f55
Xavier Craft <xcnetworking@gmail.com>
2016-07-07 21:09:40 -0700

    Create Snake.js
    Snake class defines what snake is capable of doing

---
+++ Snake.js
@@ -0,0 +1,48 @@
+var scl = 15;
+
+var food;
+
+function Snake() {
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+	this.total = 0;
+	this.tail = [];
+
+	this.update = function() {
+		for (var i = 0; i < this.total - 1; i++)
+			this.tail[i] = this.tail[i + 1];
+		}
+
+		this.tail[this.total - 1] = createVector(this.x, this.y);
+
+		this.x = this.x + this.xspeed * scl;
+		this.y = this.y + this.yspeed * scl;
+
+		this.x = constrain(this.x, 0, width - scl);
+		this.y = constrain(this.y, 0, height - scl);
+	}
+
+	this.show = function() {
+		fill(255);
+
+		for (var i = 0; i < this.total - 1; i++)
+		{
+			rect(this.tail[i].x, this.tail[i].y, scl, scl);
+		}
+		rect(this.x, this.y, scl, scl);
+	}
+
+	this.dir = function(x, y) {
+		this.xspeed = x;
+		this.yspeed = y;
+	}
+
+	this.eat = function(food) {
+		var d = dist(this.x, this.y, food.x, food.y);
+		if (d > 0) { return false; }
+		total++;
+		return true;
+	}
+}
